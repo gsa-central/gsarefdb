aa15e52ee3325fad3d3877352494b2c50422fcdf
Antonio Mora <15360867+antonio-mora@users.noreply.github.com>
2021-03-24 20:40:48 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -3,7 +3,7 @@ shinyUI(fluidPage(
   navbarPage(
     title=div(img(src="GSACENTRAL_Logo.png", width = "100px", height = "50px"),
               style = "padding-left:5px;"),
-    "GSARefDB",
+    h4("GSARefDB v1.5"),
     # inverse = TRUE,
     tags$hr(),
     ##-- Title Header ----
